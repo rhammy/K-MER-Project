2dd3682ee394d919959d673a663589f10a328f36
byrongarc <csubyrongarc@hotmail.com>
2016-08-18 15:08:52 -0700

    Update grep.c

--- grep.c
+++ grep.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
-
-
+#include<string.h>
 
 main()
 {
-char buffer[100];
-FILE *file = fopen("Tester Kmer File.txt","r");
-
-fgets(buffer,100,file);
-puts(buffer);
-
-
+	FILE *fileScanner = fopen("Tester Kmer File.txt","r");	// Opening file to begin file stream.
+	char buff[100];											// Creating a space to import data from file.
+	
+	struct sequence {										// Creating a STRUCT of type sequence. In the struct, it contains a string of the sequence. 
+		char seqString[50];									// The variable in the sequence structure is 
+	};
+	
+	struct sequence ofSequences[8];							// Creating an array of sequence STRUCTURES. 
+	
+	int counter =0; 										// Counter to keep a place in the array. 
+	while(fgets(buff,100,fileScanner)){						// Getting strings from the file one by one until no more lines avaiable.
+		strcpy(ofSequences[counter].seqString,buff);		// Copying a string from the allocated buffer space into the array of structure's string variable.
+		puts(ofSequences[counter].seqString);				// Simple print statement to display the variable from each array index. 
+		counter++;
+	}
+	
 }
 
