268f141f6dfe858b25c0132b807c43cc937ec426
byrongarc <csubyrongarc@hotmail.com>
2016-08-21 19:04:21 -0700

    Update grep.c

--- grep.c
+++ grep.c
@@ -1,23 +1,29 @@
+
 #include<stdio.h>
 #include<string.h>
 
-main()
-{
-	FILE *fileScanner = fopen("Tester Kmer File.txt","r");	// Opening file to begin file stream.
+struct sequence {					// Creating a STRUCT of type sequence. In the struct, it contains a string of the sequence. 
+		char seqString[200];	// The variable in the sequence structure is 
+	};	
+
+struct sequence ofSequences[8];		// Creating an array of sequence STRUCTURES. 
+
+void sequenceSetup(char txtFile[]){
+	FILE *fileScanner = fopen(txtFile,"r");	// Opening file to begin file stream.
 	char buff[100];											// Creating a space to import data from file.
 	
-	struct sequence {										// Creating a STRUCT of type sequence. In the struct, it contains a string of the sequence. 
-		char seqString[50];									// The variable in the sequence structure is 
-	};
-	
-	struct sequence ofSequences[8];							// Creating an array of sequence STRUCTURES. 
-	
 	int counter =0; 										// Counter to keep a place in the array. 
-	while(fgets(buff,100,fileScanner)){						// Getting strings from the file one by one until no more lines avaiable.
+	while(fgets(buff,200,fileScanner)!=NULL){						// Getting strings from the file one by one until no more lines avaiable.
+		if(buff[0]=='>'){
+			continue;
+		}else{
 		strcpy(ofSequences[counter].seqString,buff);		// Copying a string from the allocated buffer space into the array of structure's string variable.
 		puts(ofSequences[counter].seqString);				// Simple print statement to display the variable from each array index. 
 		counter++;
-	}
-	
+			}
+		}
 }
 
+main(){
+	sequenceSetup("Tester Kmer File.txt");
+}
