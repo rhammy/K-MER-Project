30562b9bf5f7d4aa2bc8b44316af6bc1f26c40ee
Reed Hamilton <hamiltonreed@ymail.com>
2016-09-06 10:56:45 -0700

    9/6/16 Grep method Planning
    Added comments on grepping kmers and sequences. Need to incorporate grep method when opening each file. I created 3 separate methods to make custom grep methods but will most likely end up writing only one grep method to use on each file in order to avoid repeated code.

--- grep.c
+++ grep.c
@@ -37,9 +37,10 @@ struct sequence {			// Creating a STRUCT of type sequence. In the struct, it con
 	};	
 
 struct sequence ofSequences[8];		// Creating an array of sequence STRUCTURES. 
+					// Need at most two lists, One to hold kmer's, second to hold sequences. 
 
-void sequenceSetup(char txtFile[]){
-	FILE *fileScanner = fopen(txtFile,"r");	// Opening file to begin file stream.
+void healthkmerGrep(char txtFile[]){		// This method will search for k-mer's with count #0 and copy that kmer into array
+	FILE *fileScanner = fopen(txtFile,"r");		// Opening file to begin file stream.
 	char buff[100];						// Creating a space to import data from file.
 	
 	int counter =0; 					// Counter to keep a place in the array. 
@@ -47,6 +48,45 @@ void sequenceSetup(char txtFile[]){
 		if(buff[0]=='>'){
 			continue;
 		}else{
+								// This line will grep for for 0's then add kmer to list. 
+		/*Grep code----------------
+		---------------------------*/
+		strcpy(ofSequences[counter].seqString,buff);	// Copying a string from the allocated buffer space into the array of structure's string variable.
+		puts(ofSequences[counter].seqString);		// Simple print statement to display the variable from each array index. 
+		counter++;
+			}
+		}
+}
+void sickkmerGrep(char txtFile[]){		// This method uses results from healthy kmer and greps to determine if present in sick kmer. 
+	FILE *fileScanner = fopen(txtFile,"r");		// Opening file to begin file stream.
+	char buff[100];						// Creating a space to import data from file.
+	
+	int counter =0; 					// Counter to keep a place in the array. 
+	while(fgets(buff,200,fileScanner)!=NULL){		// Getting strings from the file one by one until no more lines avaiable.
+		if(buff[0]=='>'){
+			continue;
+		}else{
+								// This line will grep if healthy kmer != 0 then keep that kmer in the list. If still 0, remove kmer from list. 
+		/*Grep code----------------
+		---------------------------*/
+		strcpy(ofSequences[counter].seqString,buff);	// Copying a string from the allocated buffer space into the array of structure's string variable.
+		puts(ofSequences[counter].seqString);		// Simple print statement to display the variable from each array index. 
+		counter++;
+			}
+		}
+}
+void sicksequenceGrep(char txtFile[]){		// This method will grep each kmer from fasta file, then add each sequence to a fasta file.
+	FILE *fileScanner = fopen(txtFile,"r");		// Opening file to begin file stream.
+	char buff[100];						// Creating a space to import data from file.
+	
+	int counter =0; 					// Counter to keep a place in the array. 
+	while(fgets(buff,200,fileScanner)!=NULL){		// Getting strings from the file one by one until no more lines avaiable.
+		if(buff[0]=='>'){
+			continue;
+		}else{
+								// This line will grep the sequence then add whole sequence to output file.
+		/*Grep code----------------
+		---------------------------*/
 		strcpy(ofSequences[counter].seqString,buff);	// Copying a string from the allocated buffer space into the array of structure's string variable.
 		puts(ofSequences[counter].seqString);		// Simple print statement to display the variable from each array index. 
 		counter++;
@@ -54,7 +94,13 @@ void sequenceSetup(char txtFile[]){
 		}
 }
 
-main(){
+main(int argc, char *argv[]){
+	healthy = argv[1];					//Take in 3 files from command line (healthy kmer, sick kmer, sick fasta)
+	sick = argv[2];
+	sickFST = argv[3];
+	healthkmerGrep(healthy);				//Enter each file into grep method
+	sickkmerGrep(sick);
+	sicksequenceGrep(sickFST);
 	sequenceSetup("Tester Kmer File.txt");
 	linkedListStartup();
 	addNode(1,17);
