5a61f58c47720a93577433325729bbd5409d0c12
Reed Hamilton <hamiltonreed@ymail.com>
2016-09-11 13:33:37 -0700

    Search method for healthy K-mer
    testing a search method to find k-mer that is not present in healthy txt file.

--- grep.c
+++ grep.c
@@ -38,6 +38,42 @@ struct sequence {			// Creating a STRUCT of type sequence. In the struct, it con
 
 struct sequence ofSequences[8];		// Creating an array of sequence STRUCTURES. 
 					// Need at most two lists, One to hold kmer's, second to hold sequences. 
+					
+int Search_in_File(char *fname, char *str) {
+	FILE *fp;
+	int line_num = 1;
+	int find_result = 0;
+	char temp[512];
+	
+	//gcc users
+	//if((fp = fopen(fname, "r")) == NULL) {
+	//	return(-1);
+	//}
+
+	//Visual Studio users
+	if((fopen_s(&fp, fname, "r")) != NULL) {
+		return(-1);
+	}
+
+	while(fgets(temp, 512, fp) != NULL) {
+		if((strstr(temp, str)) != NULL) {
+			printf("A match found on line: %d\n", line_num);
+			printf("\n%s\n", temp);
+			find_result++;
+		}
+		line_num++;
+	}
+
+	if(find_result == 0) {
+		printf("\nSorry, couldn't find a match.\n");
+	}
+	
+	//Close the file if still open.
+	if(fp) {
+		fclose(fp);
+	}
+   	return(0);
+}
 
 void healthkmerGrep(char txtFile[]){		// This method will search for k-mer's with count #0 and copy that kmer into array
 	FILE *fileScanner = fopen(txtFile,"r");		// Opening file to begin file stream.
@@ -49,44 +85,7 @@ void healthkmerGrep(char txtFile[]){		// This method will search for k-mer's wit
 			continue;
 		}else{
 								// This line will grep for for 0's then add kmer to list. 
-		/*Grep code----------------
-		---------------------------*/
-		strcpy(ofSequences[counter].seqString,buff);	// Copying a string from the allocated buffer space into the array of structure's string variable.
-		puts(ofSequences[counter].seqString);		// Simple print statement to display the variable from each array index. 
-		counter++;
-			}
-		}
-}
-void sickkmerGrep(char txtFile[]){		// This method uses results from healthy kmer and greps to determine if present in sick kmer. 
-	FILE *fileScanner = fopen(txtFile,"r");		// Opening file to begin file stream.
-	char buff[100];						// Creating a space to import data from file.
-	
-	int counter =0; 					// Counter to keep a place in the array. 
-	while(fgets(buff,200,fileScanner)!=NULL){		// Getting strings from the file one by one until no more lines avaiable.
-		if(buff[0]=='>'){
-			continue;
-		}else{
-								// This line will grep if healthy kmer != 0 then keep that kmer in the list. If still 0, remove kmer from list. 
-		/*Grep code----------------
-		---------------------------*/
-		strcpy(ofSequences[counter].seqString,buff);	// Copying a string from the allocated buffer space into the array of structure's string variable.
-		puts(ofSequences[counter].seqString);		// Simple print statement to display the variable from each array index. 
-		counter++;
-			}
-		}
-}
-void sicksequenceGrep(char txtFile[]){		// This method will grep each kmer from fasta file, then add each sequence to a fasta file.
-	FILE *fileScanner = fopen(txtFile,"r");		// Opening file to begin file stream.
-	char buff[100];						// Creating a space to import data from file.
-	
-	int counter =0; 					// Counter to keep a place in the array. 
-	while(fgets(buff,200,fileScanner)!=NULL){		// Getting strings from the file one by one until no more lines avaiable.
-		if(buff[0]=='>'){
-			continue;
-		}else{
-								// This line will grep the sequence then add whole sequence to output file.
-		/*Grep code----------------
-		---------------------------*/
+		
 		strcpy(ofSequences[counter].seqString,buff);	// Copying a string from the allocated buffer space into the array of structure's string variable.
 		puts(ofSequences[counter].seqString);		// Simple print statement to display the variable from each array index. 
 		counter++;
@@ -95,21 +94,13 @@ void sicksequenceGrep(char txtFile[]){		// This method will grep each kmer from 
 }
 
 main(int argc, char *argv[]){
-	healthy = argv[1];					//Take in 3 files from command line (healthy kmer, sick kmer, sick fasta)
-	sick = argv[2];
-	sickFST = argv[3];
-	healthkmerGrep(healthy);				//Enter each file into grep method
-	sickkmerGrep(sick);
-	sicksequenceGrep(sickFST);
-	sequenceSetup("Tester Kmer File.txt");
-	linkedListStartup();
-	addNode(1,17);
-	addNode(1,61);
-	addNode(1,93);
-	addNode(2,5);
-	addNode(2,12);
-	addNode(2,15);
-	printIndex(1);
-	printIndex(2);
-	
+	result = Search_in_File(argv[1], argv[2]);
+	if(result == -1) {
+		perror("Error");
+		printf("Error number = %d\n", errno);
+		exit(1);
+	}
+	return(0);
+}
+
 }
