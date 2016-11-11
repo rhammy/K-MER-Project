e3eddf74a205100b334b49187bb6454bb7922ae0
Reed Hamilton <hamiltonreed@ymail.com>
2016-11-11 11:55:25 -0800

    new hash

---
+++ hash.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+unsigned long
+hash(unsigned char *str)
+{
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash;
+}
+
+int main() {
